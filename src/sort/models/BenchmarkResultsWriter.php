8901e8ba1a0550117581a86c5ecc3a5a966522da
Romain Ciaccafava <rciaccafava@gmail.com>
2015-02-10 16:55:23 +0100

    Refactored model using PDO

--- src/sort/models/BenchmarkResultsWriter.php
+++ src/sort/models/BenchmarkResultsWriter.php
@@ -32,6 +32,14 @@ class BenchmarkResultsWriter
      */
     public function write($sortName, $numberOfElements, $microtime)
     {
-        $this->dbConnection->exec('INSERT INTO benchmark (benchmark_name, algorithm, number_of_elements, microtime) VALUES (?, ?, ?, ?)', array($this->conf['benchmark']['name'], $sortName, $numberOfElements, $microtime));
+        $query = $this->dbConnection->prepare('INSERT INTO benchmark (benchmark_name, algorithm, number_of_elements, microtime) VALUES (:benchmark_name, :algorithm, :number_of_elements, :microtime)');
+        $query->execute(
+            array(
+                ':benchmark_name' => $this->conf['benchmark']['name'],
+                ':algorithm' => $sortName,
+                ':number_of_elements' => $numberOfElements,
+                ':microtime' => $microtime
+            )
+        );
     }
 } 
